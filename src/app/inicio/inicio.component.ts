2666f1a77a0ccda746e15821bc9f4f0b65e20c27
Ícaro <allegarcezz@gmail.com>
2021-08-04 21:27:13 -0300

    Configurações de início completas e funcionais.

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    /*if (environment.token == '') {
+      /*alert('Sua sessão expirou, faça o login novamente.');
+      this.router.navigate(['/login']);*/
+    }
   }
-
-}
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,26 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/usuario';
 import { UsuarioLogin } from '../model/usuarioLogin';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
-    return this.http.post<UsuarioLogin>('https://vikare.herokuapp.com/usuarios/logar', usuarioLogin)
+    return this.http.post<UsuarioLogin>(
+      'https://vikare.herokuapp.com/usuarios/logar',
+      usuarioLogin
+    );
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>('https://vikare.herokuapp.com/usuarios/cadastrar', usuario)
-
+    return this.http.post<Usuario>(
+      'https://vikare.herokuapp.com/usuarios/cadastrar',
+      usuario
+    );
   }
 
+  logado() {
+    let ok = false;
+    if (environment.token != '') {
+      ok = true;
+    }
+    return ok;
+  }
 }
-
